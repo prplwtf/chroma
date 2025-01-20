8a3303aa9a682130cdd96bc5901fba50fb35d172
Emma <hello@prpl.wtf>
2025-01-20 17:27:56 +0100

    feat: first steps

--- src/index.js
+++ src/index.js
@@ -17,7 +17,21 @@ class Chroma {
   init() {
     // Setup code
     console.log('Initializing on', this.element);
-    this.element.classList.add('chroma:field');
+    this.element.setAttribute('_chroma', 'input');
+
+    // Create label element
+    const label = document.createElement('label');
+    label.setAttribute('_chroma', 'input');
+
+    // Set the 'for' attribute to match the input's ID
+    if (!this.element.id) {
+      // Generate a unique ID if none exists
+      this.element.id = `chroma-input-${Math.random().toString(36).substr(2, 9)}`;
+    }
+    label.setAttribute('for', this.element.id);
+
+    // Insert label before the input element (instead of prepending to the element)
+    this.element.parentNode.insertBefore(label, this.element);
   }
 }
 
--- src/theme.js
+++ src/theme.js
@@ -4,14 +4,35 @@ function theme() {
 
   // Define styles
   const style = `
-    :root {
-      --chroma-input-border: 1px black solid;
-      --chroma-input-radius: 12px;
+    [_chroma] {
+      --chroma-input-border-width: 1px;
+      --chroma-input-border-color: black;
+      --chroma-input-border-style: solid;
+      --chroma-input-border-radius: 12px;
+      --chroma-input-height: 20px;
+      --chroma-input-color: black;
+      --chroma-input-padding: 1px;
     }
 
-    input.chroma\\:field {
-      border-radius: var(--chroma-input-radius);
-      border: var(--chroma-input-border);
+    input[_chroma="input"],
+    label[_chroma="input"] {
+      border-width: var(--chroma-input-border-width);
+      border-color: var(--chroma-input-border-color);
+      border-style: var(--chroma-input-border-style);
+      padding: var(--chroma-input-padding);
+    }
+
+    input[_chroma="input"] {
+      border-radius: 0px var(--chroma-input-border-radius) var(--chroma-input-border-radius) 0px;
+      height: var(--chroma-input-height);
+    }
+
+    label[_chroma="input"] {
+      border-radius: var(--chroma-input-border-radius) 0px 0px var(--chroma-input-border-radius);
+      float: left;
+      width: 20px;
+      height: var(--chroma-input-height);
+      background-color: var(--chroma-input-color);
     }
   `;
 
